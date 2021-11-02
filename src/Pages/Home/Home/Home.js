7b4157f37525afa844d319b3e09d70de6a73da13
MD NAZIM UDDIN <nushourav4@gmail.com>
2021-11-02 11:03:38 +0900

    initial

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../../Sheared/Navigation/Navbar';
+import Services from '../Services/Services';
+
+const Home = () => {
+    return (
+        <div>
+           <Navbar></Navbar>
+           <Services/>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,77 @@
+import { CardActionArea, Container } from '@mui/material';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import cavity from '../../../images/cavity.png';
+import fluoride from '../../../images/fluoride.png';
+import whitening from '../../../images/whitening.png';
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  const services = [
+    {
+        name: 'Fluoride Treatment',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas',
+        img: fluoride
+    },
+    {
+        name: 'Cavity Filling',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas',
+        img: cavity
+    },
+    {
+        name: 'Teeth Whitening',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed culpa cumque enim! Voluptatibus aliquid expedita saepe accusantium itaque ducimus rem voluptas',
+        img: whitening
+    }
+]
+
+const Services = () => {
+    return (
+        <Box sx={{ flexGrow: 1 , my:5}}>
+        <Container>
+        <Typography variant="h6" color="text.secondary">
+            our services
+          </Typography>
+          <Typography variant="h4" color="" sx={{mb:2}}>
+            service we provide
+          </Typography>
+        <Grid container spacing={{ xs: 2, md: 3}} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {services.map (service=><Grid item xs={2} sm={4} md={4} key={service.name}>
+            <Card sx={{ maxWidth: 345,border:0, boxShadow: 0  }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          style={{height:80,width:"auto",margin:" 0 auto"}}
+          image={service.img}
+          alt={service.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {service.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {service.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+            </Grid>
+          )}
+        </Grid>
+        </Container>
+      </Box>
+    );
+};
+
+export default Services;
